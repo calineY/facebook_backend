1506e18f18a6f66245d83b799af52bb1fa4cbb13
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-02-13 21:15:01 +0200

    Add: user can upload a profile picture

--- PHP/signup.php
+++ PHP/signup.php
@@ -3,11 +3,14 @@
 include("db_info.php");
 
 
-if(isset($_POST["email"]) && isset($_POST["user_name"]) && isset($_POST["password"]) && isset($_POST["password_check"])){
+if(isset($_POST["email"]) && isset($_POST["user_name"]) && isset($_POST["password"]) && isset($_POST["password_check"]) && isset($_FILES['file']['name'])){
     $email = $mysqli->real_escape_string($_POST["email"]);
     $name = $mysqli->real_escape_string($_POST["user_name"]);
     $password = $mysqli->real_escape_string($_POST["password"]); 
     $password = hash("sha256",$password);
+    $image_name = $_FILES['file']['name'];
+    $valid_extensions = array("jpg","jpeg","png");
+    $extension = pathinfo($image_name, PATHINFO_EXTENSION);
 }else{
     die("Please fill all fields.");
 }
@@ -19,10 +22,19 @@ $query->store_result();
 if(($query->num_rows)>0){
     die("Email already exists");
 }else{
-    $query = $mysqli->prepare("INSERT INTO user(user_name,user_email,password) VALUES (?,?,?)"); 
-    $query->bind_param("sss", $name, $email, $password);
-    $query->execute();
+    if(in_array($extension, $valid_extensions)){
+        $image_name = time() . '.' . $extension;
+        $upload_path = '../upload/'. $image_name;
 
+        $query = $mysqli->prepare("INSERT INTO user(user_name,user_email,password,user_picture) VALUES (?,?,?,'$image_name')"); 
+        $query->bind_param("sss", $name, $email, $password);
+        $query->execute();
+
+        move_uploaded_file($_FILES['file']['tmp_name'], $upload_path);
+        
+    }else {
+        die("Please enter a valid picture that ends with ('.jpg','.jpeg','.png')");
+    }
     echo "User created";
 }
 $query->close();
