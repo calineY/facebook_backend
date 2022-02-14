cff6689c7fd25613e969b132cb15292819f4fed9
Caline <calineyammine123@gmail.com>
2022-02-14 14:41:09 +0200

    Add: send friend request to a user

---
+++ PHP/send_request.php
@@ -0,0 +1,27 @@
+<?php
+
+include("db_info.php");
+
+
+if(isset($_GET["user_id"])){
+    $id = $_GET["user_id"];
+    // decrypt id
+    // ------------------------
+}else {
+    die("ID was not Received!");
+}
+
+if(isset($_GET["to_user_id"])){
+    $request_to_id = $_GET["to_user_id"];
+}else {
+    die("ID to request was not Received!");
+}
+
+$query = $mysqli->prepare("INSERT INTO friend_request(from_user_id,to_user_id) VALUES (?,?)"); 
+$query->bind_param("ii", $id, $request_to_id);
+$query->execute();
+
+echo "Request sent";
+$query->close();
+$mysqli->close();
+?>
