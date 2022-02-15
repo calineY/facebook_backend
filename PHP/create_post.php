2576ed7130f9f04a914b5720f721a03a7fd84ffb
Caline <calineyammine123@gmail.com>
2022-02-15 14:24:26 +0200

    Add: PHP file to delete a post

--- PHP/create_post.php
+++ PHP/create_post.php
@@ -17,7 +17,7 @@ if(isset($_GET["post_content"])){
     die("Fill post content.");
 }
 
-$post_date = date("Y-m-d H:i:s", strtotime ("+1 hour"));;
+$post_date = date("Y-m-d H:i:s", strtotime ("+1 hour"));
 
 
 $query = $mysqli->prepare("INSERT INTO post (post_content,post_date,user_id) VALUES (?, ?,?);"); 
