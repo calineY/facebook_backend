7f0818d74bfff69369de118ad22100a684ed2349
Caline <calineyammine123@gmail.com>
2022-02-15 12:15:56 +0200

    Add: PHP file to handle like and unlike post from user

--- PHP/block_unblock.php
+++ PHP/block_unblock.php
@@ -12,7 +12,7 @@ if(isset($_GET["user_id"])){
 }
 
 if(isset($_GET["to_user_id"])){
-    $friend_id = $_GET["to_user_id"];
+    $to_user_id = $_GET["to_user_id"];
 }else {
     die("ID to block/unblock was not Received!");
 }
@@ -27,7 +27,7 @@ if(isset($_GET["action"])){
 
 if ($action=="block"){ 
     $query = $mysqli->prepare("INSERT INTO block (from_user_id,to_user_id) VALUES (?, ?);"); 
-    $query->bind_param("ii", $id, $friend_id);
+    $query->bind_param("ii", $id,$to_user_id);
     $query->execute();
 
     echo "User blocked";
@@ -35,12 +35,11 @@ if ($action=="block"){
 
 if ($action=="unblock"){
     $query2 = $mysqli->prepare("DELETE FROM block WHERE from_user_id=? AND to_user_id=?;"); 
-    $query2->bind_param("ii",$id,$friend_id);
+    $query2->bind_param("ii",$id,$to_user_id);
     $query2->execute();
 
     echo "User unblocked";
 }  
     
-
 
 ?>
