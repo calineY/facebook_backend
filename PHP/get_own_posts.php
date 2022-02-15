1ed7f2ca5bd222fa5ce39569059c70b70556259d
Caline <calineyammine123@gmail.com>
2022-02-15 18:20:06 +0200

    Add: get user info and own posts

--- PHP/get_own_posts.php
+++ PHP/get_own_posts.php
@@ -10,12 +10,12 @@
         die("ID was not Received!");
     }
 
-    $query=$mysqli->prepare("SELECT users.user_picture,users.user_name,posts.post_id,posts.post_content,posts.post_date,
+    $query=$mysqli->prepare("SELECT posts.post_id,posts.post_content,posts.post_date,
     (SELECT COUNT(post_likes.id) from post_likes WHERE post_likes.post_id=posts.post_id)
-     AS nb_likes 
-     FROM posts,users 
-     WHERE posts.user_id IN(SELECT friend_id FROM user_friends WHERE user_id=? UNION SELECT user_id FROM user_friends WHERE friend_id=? UNION SELECT user_id FROM users WHERE user_id=?) AND users.user_id=posts.user_id ORDER BY posts.post_date DESC;");
-    $query->bind_param("iii",$id,$id,$id);
+    AS nb_likes 
+    FROM posts,users
+    WHERE posts.user_id IN(SELECT user_id FROM users WHERE user_id=?) AND users.user_id=posts.user_id ORDER BY posts.post_date DESC;");
+    $query->bind_param("s", $id);
     $query->execute();
     $array = $query->get_result();
 
